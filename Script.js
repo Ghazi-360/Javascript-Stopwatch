653c29203c76d59b7746c052016f4333e9862ff9
Ghazi 360 <muazwaseem88@gmail.com>
2021-01-01 12:36:25 +0500

    Final Changes

---
+++ Script.js
@@ -0,0 +1,63 @@
+window.onload = function() {
+
+    //Create variables for elements to be used
+
+    var seconds = 00;
+    var tens = 00;
+    var appendTens = document.getElementById("Tens")
+    var appendSeconds = document.getElementById("Seconds")
+    var startbutton = document.getElementById("StartButton");
+    var stopbutton = document.getElementById("StopButton"); 
+    var resetbutton = document.getElementById("ResetButton");
+    var interval;
+
+    //Set Events for buttons
+
+    startbutton.onclick = function() {
+        clearInterval(interval);
+        //Interval for the timer to start
+        interval = setInterval (startTimer, 10);
+    }
+
+    //when pressed, interval is cleared
+    stopbutton.onclick = function() {
+        clearInterval(interval);
+    } 
+
+    //when pressed,clears the interal & sets the values to zero.
+    resetbutton.onclick = function() {
+        clearInterval(interval);
+        tens= "00";
+        seconds= "00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+
+
+    function startTimer() {
+        //Increments tens(00) by 1 each time.
+        tens++;
+
+        //untill < 9, the left zero remains unchanged. value of right zero keeps varrying
+        if (tens < 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+        //when > 9, value of both figures varries.
+        if (tens > 9) {
+             appendTens.innerHTML = tens;
+        }
+        // when reaches 99, starts incrementing the value of seconds 1 by 1
+        if (tens > 99) {
+            seconds++;
+             appendSeconds.innerHTML = "0" + seconds;
+             tens = 0;
+            appendTens.innerHTML = "0" + 0;
+        }
+        //when > 9, value of both figures varries.
+        if (seconds > 9) {
+            appendSeconds.innerHTML = seconds;
+         }
+    }
+
+}
+
